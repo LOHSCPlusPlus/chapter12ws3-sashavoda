c0458702e0252052b061509ed2f73e09354dcdb2
SASHAVODA2 <>
2022-04-26 15:34:03 +0000

    ws3 done

--- ws3.cpp
+++ ws3.cpp
@@ -1,3 +1,4 @@
+//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
 #include <fstream>
 #include <iostream>
 #include <cstring>
@@ -7,40 +8,49 @@
 using namespace std;
 
 void PartA() {
-    // Create 2 pointers to int called ptr1 and ptr2. 
-    // Set them both to nullptr when you create them
-
+int  *ptr1 = nullptr;
+int  *ptr2 = nullptr;
 
     // Create 1 pointer to a char called charPtr
+char *charPtr;
     // set it to nullptr when you create it.
-
+  charPtr = nullptr;
     
     // Create two integers named size1 and size2.
-
-
+  int size1;
+  int size2;
     // Prompt the user for 2 array sizes, and read them from 
+  cout << "Enter 2 array sizes: ";
     // the user into size1 and size2 (standard cout and cin flow).
-
+  cin >> size1 >> size2;
+  ptr1 = new int[size1];
+  ptr2 = new int[size2];
 
     // Fill the two arrays with random numbers using two different for loops.
     // To generate a random number use rand(); 
     // For example:
-    // ptr1[index] = rand();
-    
-    
-
+  for(int index = 0; index < size1; index++){
+     ptr1[index] = rand();
+    }
+  for(int i = 0; i < size2; i++){
+    ptr2[i] = rand();
+  }
     
     // Using new, create an array of size 51 and set charPtr equal to it.
-
+  charPtr = new char[51];
     
     // Prompt the user for a sentence of less than 50 words. 
+  cout << "Enter a sentence 50 words or less";
     // After the prompt uncomment this code to read the sentence.
 
-    // cin.get(charPtr, 51);
+     cin.get(charPtr, 51);
 
     // print the string the user just entered using cout.
-
+  cout << charPtr;
     // Use delete[] to free the memory for ptr1, ptr2 and charPtr
+  delete [] ptr1;
+  delete [] ptr2;
+  delete [] charPtr;
     // Build the file and run it
     // make -f ws3.mak
     // ./ws3
